f01e33ac10f3566e17ab73416466aeecbcf5c054
nikhilmantri99 <mantri.dpn09@iitkgp.ac.in>
2022-02-07 10:52:34 +0530

    added DynamoDB support

--- handler.js
+++ handler.js
@@ -1,13 +1,10 @@
+import AWS from "aws-sdk";
 const needle = require('needle');
-// The code below sets the bearer token from your environment variables
-// To set environment variables on macOS or Linux, run the export command below from the terminal:
-// export BEARER_TOKEN='YOUR-TOKEN'
 const token = "AAAAAAAAAAAAAAAAAAAAAP0pYwEAAAAA7tbTpU6l5L7ypGNweYs6JsydUuI%3DsnKPijOSWqlF3AccVRyYiju59XP258x2QHyWbluCRxbDpiVVHo";
 const endpointURL = "https://api.twitter.com/2/users/by?usernames=";
 
+
 async function getRequest(username_) {
-    var todayDate = new Date().toISOString().slice(0, 10);
-    console.log(todayDate);
     // These are the parameters for the API request
     // specify User names to fetch, and any additional fields that are required
     // by default, only the User ID, name and user name are returned
@@ -25,16 +22,67 @@ async function getRequest(username_) {
         }
     });
 
-    if (res.body) {
-        return res.body;
-    } else {
-        throw new Error('Unsuccessful request');
+    if (res.body==null) {
+        return new Error('Unsuccessful request');
     }
+    var todayDate = new Date().toISOString().slice(0, 10);
+    console.log(todayDate);
+    const new_val={
+        username : res.body.data[0].username,
+        name: res.body.data[0].name,
+        id: res.body.data[0].id,
+        date: todayDate,
+        followers_count: res.body.data[0].public_metrics.followers_count,
+        following_count: res.body.data[0].public_metrics.following_count,
+        tweet_count: res.body.data[0].public_metrics.tweet_count,
+    };
+    AWS.config.update({region:'us-east-1'});
+    const dynamoDb = new AWS.DynamoDB.DocumentClient();
+    const get_back = {
+        TableName: "store-twitter-follower-info",
+        Key: {
+            username: res.body.data[0].username, // The id of the author
+        },
+    };
+    const oldresult = await dynamoDb.get(get_back).promise();
+    var ls=[];
+    if(oldresult!=null && oldresult.Item!=null){
+        ls=oldresult.Item.info;
+        var n=ls.length;
+        if(ls[n-1].date!=new_val.date){
+            ls.push(new_val);
+        }
+        else{
+            ls[n-1]=new_val;
+        }
+    }
+    else{
+        ls.push(new_val);
+    }
+    const paramsnew={
+        TableName: get_back.TableName,
+        Item:{
+            username: get_back.Key.username,
+            info: ls,
+        }
+    }
+    try{
+        await dynamoDb.put(paramsnew).promise();
+    }
+    catch(e){
+        console.log("Error is found....");
+        console.log(e);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: e.message }),
+        };
+    }
+    return ls;
 }
 
 export const hello = async (event, context) => {
-  //const ans= await getRequest(event["queryStringParameters"]['UserName']);
-  const ans= await getRequest('joebiden');
+  const ans= await getRequest(event["queryStringParameters"]['UserName']);
+  //const ans= await getRequest('joebiden');
   return {
     statusCode: 200,
     body: JSON.stringify(ans,null,2),
